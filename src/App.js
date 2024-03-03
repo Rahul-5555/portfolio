b57bd48d6e3247407d16a0171cddc1848db11205
Rahul-5555 <rahul.malviya676@gmail.com>
2024-03-03 16:09:14 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utils/Themes';
+import Navbar from './Components/Navbar';
+import Hero from './Components/HeroSection';
+import Skills from './Components/Skills';
+import Education from './Components/Education/Index';
+import { BrowserRouter as Router } from "react-router-dom";
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Footer from "./Components/Footer";
+
+const Body = styled.div`
+background-color: ${({ theme }) => theme.bg};
+height: 100%;
+width: 100%;
+overflow-x: hidden;`;
+
+const Wrapper = styled.div`
+background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+width: 100%;
+clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+          </Wrapper>
+          <Projects />
+          <Wrapper>
+            <Education />
+            <Contact/>
+          </Wrapper>
+
+          <Footer/>
+    
+        </Body>
+      </Router>
+
+    </ThemeProvider>
   );
 }
 
