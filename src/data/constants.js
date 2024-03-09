c00a88ac23e3e19cd01285f4e30381b5541436c1
Rahul-5555 <rahul.malviya676@gmail.com>
2024-03-09 10:59:32 +0530

    Add VidTube Project

--- src/data/constants.js
+++ src/data/constants.js
@@ -278,7 +278,7 @@ export const projects = [
     ],
     category: "web app",
     github: "https://github.com/Rahul-5555/E-Commerce",
-    webapp: "https://trackify.duckdns.org",
+    webapp: "https://github.com/Rahul-5555/E-Commerce",
   },
   {
     id: 0,
@@ -290,29 +290,24 @@ export const projects = [
     tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
     category: "web app",
     github: "https://github.com/Rahul-5555/wanderlist",
-    webapp: "https://podstream.netlify.app/",
+    webapp: "https://github.com/Rahul-5555/wanderlist",
    
   },
-  // {
-  //   id: 1,
-  //   title: "Vexa",
-  //   date: "Oct 2022 - Present",
-  //   description:
-  //     "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
-  //   tags: [
-  //     "React Js",
-  //     "MongoDb",
-  //     "Node Js",
-  //     "Express Js",
-  //     "Redux",
-  //     "NodeMailer",
-  //   ],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Project-Management-App",
-  //   webapp: "https://vexa-app.netlify.app/",
-  // },
+  {
+    id: 1,
+    title: "VidTube",
+    date: "Feb 2024 - March 2024",
+    description:
+      "A simple YouTube clone project built using React. It allows users to watch videos and view video details such as title, view count, likes, dislikes, and comments. The project uses the YouTube Data API for fetching video data and includes features like video autoplay, channel information, subscriber count, and comment display.",
+    image:
+      "https://github.com/Rahul-5555/YouTube-Clone/blob/59bc694be04e1557a5ebf01a2661ed3d092efdee/vidtube.png?raw=true",
+    tags: [
+      "React Js",
+    ],
+    category: "web app",
+    github: "https://github.com/Rahul-5555/YouTube-Clone",
+    webapp: "https://rahulyoutube-clone.netlify.app/",
+  },
   // {
   //   id: 2,
   //   title: "Brain Tumor Detection",
