7f8ddaceeac78954a6c2e95cc3e16e1ae1037ed6
Rahul-5555 <rahul.malviya676@gmail.com>
2025-04-23 10:50:28 +0530

    add linkedin project

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,46 +1,43 @@
 import React from 'react';
 import { Link as LinkR } from "react-router-dom";
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { DiCssdeck } from "react-icons/di";
 import { FaBars } from "react-icons/fa";
 import { Bio } from '../../data/constants';
-import { useTheme } from 'styled-components';
+
+// Styled Components
 
 const Nav = styled.div`
-background-color: ${({ theme }) => theme.card_light};
-height: 80px;
-display: flex;
-align-items: center;
-justify-content: center;
-font-size: 1rem;
-position: sticky;
-top: 0;
-z-index: 10;
-@media (max-width: 960px) {
-    trastion: 0.8s all ease;
-}`;
+  background-color: ${({ theme }) => theme.card_light};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  @media (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
 
 const NavbarContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-height: 60px;
-z-index: 1;
-width: 100%;
-padding: 0 24px;
-max-width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1200px;
 `;
 
 const NavLogo = styled(LinkR)`
-    width: 80%;    
-    padding: 0 6px;
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    text-decoration: none;
-    @media (max-width: 640px) {
-      padding: 0 0px;
-  }
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text_primary};
 `;
 
 const MobileIcon = styled.div`
@@ -58,41 +55,35 @@ const MobileIcon = styled.div`
 `;
 
 const NavItems = styled.ul`
-width: 100%;
-display: flex;
-align-items: center;
-justify-content:center;
-gap: 32px;
-padding: 0 6px;
-list-style: none;
-
-@media screen and (max-width: 768px) {
-  display: none;
-}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
-color: ${({ theme }) => theme.text_primary};
-font-weight: 500;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
-:hover {
-  color: ${({ theme }) => theme.primary};
-}
-
-&.active {
-  border-bottom: 2px solid ${({ theme }) => theme.primary};
-}
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.primary};
+  }
 `;
 
 const ButtonContainer = styled.div`
-  width: 80%;  
-  height: 100%;
   display: flex;
-  justify-content: end;
+  justify-content: flex-end;
   align-items: center;
-  padding: 0 6px;
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -101,9 +92,9 @@ const ButtonContainer = styled.div`
 const GitHubLink = styled.a`
   background-color: transparent;
   border: 1.8px solid ${({ theme }) => theme.primary};
-  justify-content: center;
   display: flex;
   align-items: center;
+  justify-content: center;
   height: 70%;
   border-radius: 20px;
   color: ${({ theme }) => theme.primary};
@@ -113,115 +104,119 @@ const GitHubLink = styled.a`
   text-decoration: none;
   font-size: 16px;
   transition: all 0.6s ease-in-out;
-    :hover {
-      background: ${({ theme }) => theme.primary};
-      color: ${({ theme }) => theme.white};     
-    }
-    @media screen and (max-width: 640px) { 
+  &:hover {
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.white};
+  }
+  @media screen and (max-width: 640px) {
     font-size: 14px;
-    }
+  }
 `;
 
 export const Span = styled.div`
-padding: 0 4px;
-font-weight: bold;
-font-size: 18px;
+  padding: 0 4px;
+  font-weight: bold;
+  font-size: 18px;
 `;
 
 const MobileMenu = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-gap: 16px;
-position: absolute;
-top: 80px;
-right: 0;
-width: 100%;
-padding: 12px 40px 24px 40px;
-background: ${({ theme }) => theme.card_light + 99};
-transition: all 0.6s ease-in-out;
-transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
-border-radius: 0 0 20px 20px;
-box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
-opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
-`
-const MobileMenuLink = styled(LinkR)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  position: absolute;
+  top: 80px;
+  right: 0;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.card_light + 99};
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+  z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
+`;
+
+const MobileMenuLink = styled.a`
   color: ${({ theme }) => theme.text_primary};
   font-weight: 500;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-  :hover {
+  &:hover {
     color: ${({ theme }) => theme.primary};
   }
-
   &.active {
     border-bottom: 2px solid ${({ theme }) => theme.primary};
   }
 `;
 
-
+// Navbar Component
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
+
   return (
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-        <LinkR style={{ display: "flex", alignItems: "center", color: "white", marginBottom: 20, cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
-          </LinkR>
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
         </NavLogo>
 
         <MobileIcon>
           <FaBars
-            onClick={() => {
-              setOpen(!open);
-            }}
+            aria-label="Toggle mobile menu"
+            onClick={() => setOpen(!open)}
           />
         </MobileIcon>
+
         <NavItems>
           <NavLink href="#about">About</NavLink>
-          <NavLink href='#skills'>Skills</NavLink>
-          {/* <NavLink href='#experience'>Experience</NavLink> */}
-          <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Education</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
         </NavItems>
+
         <ButtonContainer>
-          <GitHubLink href={Bio.github} target="_blank">Github Profile</GitHubLink>
+          <GitHubLink href={Bio.github} target="_blank">
+            Github Profile
+          </GitHubLink>
         </ButtonContainer>
       </NavbarContainer>
-      {
-        open && (
-          <MobileMenu open={open}>
-            <MobileMenuLink href="#about"
-              onClick={() => {
-                setOpen(!open);
-              }}
-            >
-              About
-            </MobileMenuLink>
-            <MobileMenuLink href='#skills' onClick={() => {
-              setOpen(!open);
-            }}>Skills</MobileMenuLink>
-            {/* <MobileMenuLink href='#experience' onClick={() => {
-              setOpen(!open);
-            }}>Experience</MobileMenuLink> */}
-            <MobileMenuLink href='#projects' onClick={() => {
-              setOpen(!open);
-            }}>Projects</MobileMenuLink>
-            <MobileMenuLink href='#education' onClick={() => {
-              setOpen(!open);
-            }}>Education</MobileMenuLink>
-            <GitHubLink style={{ padding: '10px 16px', background: `${theme.primary}`, color: 'white', width: 'max-content' }} href="/" target="_blank">
-              Github Profile</GitHubLink>
-          </MobileMenu>
-        )
-      }
+
+      {open && (
+        <MobileMenu isOpen={open}>
+          <MobileMenuLink href="#about" onClick={() => setOpen(false)}>
+            About
+          </MobileMenuLink>
+          <MobileMenuLink href="#skills" onClick={() => setOpen(false)}>
+            Skills
+          </MobileMenuLink>
+          <MobileMenuLink href="#projects" onClick={() => setOpen(false)}>
+            Projects
+          </MobileMenuLink>
+          <MobileMenuLink href="#education" onClick={() => setOpen(false)}>
+            Education
+          </MobileMenuLink>
+
+          <GitHubLink
+            style={{
+              padding: '10px 16px',
+              background: `${theme.primary}`,
+              color: 'white',
+              width: 'max-content',
+            }}
+            href={Bio.github}
+            target="_blank"
+          >
+            Github Profile
+          </GitHubLink>
+        </MobileMenu>
+      )}
     </Nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/data/constants.js
+++ src/data/constants.js
@@ -286,6 +286,28 @@ export const projects = [
   },
 
   {
+    id: 7,
+    title: "Linkedin",
+    date: "Feb 2025 -  April 2025",
+    description:
+      "I developed a LinkedIn-inspired social networking platform using the MERN stack, featuring secure user authentication with JWT and Bcrypt, post creation with text and image uploads, real-time friend requests and notifications via Socket.io, and interactive engagement features like liking and commenting on posts. Built with React, Node.js, Express, and MongoDB, this full-stack project demonstrates my ability to integrate real-time functionality while maintaining secure and scalable architecture. The app showcases my proficiency in handling complex backend logic, responsive frontend design, and seamless WebSocket communication—perfectly highlighting my full-stack development capabilities.",
+    image: "https://github.com/Rahul-5555/Linkedin-/blob/main/Linkdedin.png?raw=true",
+    tags: [
+      "Html",
+      "Tailwind Css",
+      "Javascript",
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+    github: "https://github.com/Rahul-5555/Linkedin-",
+    webapp: "https://linkedin-frontend-2lrq.onrender.com",
+  },
+
+
+  {
     id: 8,
     title: "Job Quest",
     date: "July 2024 - September 2024",
@@ -307,9 +329,10 @@ export const projects = [
     github: "https://github.com/Rahul-5555/Job-Portal",
     webapp: "https://jobportalrah.netlify.app/",
   },
+
   {
     id: 9,
-    title: "E-commerse-web",
+    title: "E-commerce-web",
     date: "May 2024 - June 2024",
     description:
       "Welcome to E-commerce-web! Enjoy a seamless shopping experience with our platform's intuitive add-to-cart functionality, robust user authentication, and efficient MongoDB-powered database management. Our responsive design adapts to various devices, ensuring an optimized browsing experience. Additionally, benefit from our powerful search filter functionality to easily find the products you need. Shop confidently knowing your data is secure and your satisfaction is our priority.",
@@ -328,27 +351,7 @@ export const projects = [
     github: "https://github.com/Rahul-5555/E-commerce-1",
     webapp: "https://payment-order-e-commerce-web-4p7d.vercel.app/",
   },
-  // {
-  //   id: 9,
-  //   title: "E-commerse",
-  //   date: "Jun 2023 - Jul 2023",
-  //   description:
-  //     "Welcome to [Your E-commerce ]! Our platform offers a seamless shopping experience with features including intuitive add-to-cart functionality, robust user authentication ensuring security and privacy, efficient database management powered by MongoDB for storing and retrieving vast product inventory and user data, and a responsive design that adapts flawlessly to various devices, ensuring a consistent and optimized browsing experience. Shop confidently knowing your data is secure and your satisfaction is our priority.",
-  //   image: "https://i.ytimg.com/vi/xun2w12nW1U/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAqRcFRBtc94QzfpX4JuQmnk1q7zQ",
-  //   tags: [
-  //     "Html",
-  //     "Css",
-  //     "Javascript",
-  //     "React Js",
-  //     "MongoDb",
-  //     "Node Js",
-  //     "Express Js",
-  //     "Redux",
-  //   ],
-  //   category: "web app",
-  //   github: "https://github.com/Rahul-5555/E-Commerce",
-  //   webapp: "https://github.com/Rahul-5555/E-Commerce",
-  // },
+
   {
     id: 0,
     title: "Wanderlust",
@@ -362,6 +365,7 @@ export const projects = [
     webapp: "https://github.com/Rahul-5555/wanderlist",
 
   },
+
   {
     id: 1,
     title: "VidTube",
@@ -377,6 +381,7 @@ export const projects = [
     github: "https://github.com/Rahul-5555/YouTube-Clone",
     webapp: "https://rahulyoutube-clone.netlify.app/",
   },
+
   {
     id: 2,
     title: "Thread-clone",
@@ -391,103 +396,13 @@ export const projects = [
     webapp: "https://thread-clone-1.onrender.com"
   },
 
-  // {
-  //   id: 3,
-  //   title: "Buckoid",
-  //   date: "Dec 2021 - Apr 2022",
-  //   description:
-  //     "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
-  //   image:
-  //     "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
-  //   tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
-  //   category: "android app",
-  //   github: "https://github.com/rishavchanda/Buckoid-Android-App",
-  //   webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
-  // },
-  // {
-  //   id: 10,
-  //   title: "Job Finding App",
-  //   date: "Jun 2023 - Jul 2023",
-  //   description:
-  //     "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
-  //   image:
-  //     "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
-  //   tags: ["React Native", "JavaScript", "Axios"],
-  //   category: "android app",
-  //   github: "https://github.com/rishavchanda/Job-finder-App",
-  //   webapp: "https://github.com/rishavchanda/Job-finder-App",
-  // },
-  // {
-  //   id: 4,
-  //   title: "Whatsapp Clone",
-  //   date: "Jul 2021",
-  //   description:
-  //     "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
-  //   image:
-  //     "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
-  //   tags: ["React Js", "Firebase", "Firestore", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
-  //   webapp: "https://whatsapp-clone-rishav.web.app",
-  // },
-  // {
-  //   id: 5,
-  //   title: "Todo Web App",
-  //   date: "Jun 2021",
-  //   description:
-  //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
-  //   image:
-  //     "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Todo-Web-App",
-  //   webapp: "https://rishav-react-todo.netlify.app/",
-  // },
-  // {
-  //   id: 6,
-  //   title: "Breaking Bad",
-  //   date: "Jun 2021",
-  //   description:
-  //     "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
-  //   image:
-  //     "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
-  //   tags: ["React Js", "API", "Axios", "Node JS"],
-  //   category: "web app",
-  //   github: "https://github.com/rishavchanda/Breaking-Bad",
-  //   webapp: "https://breaking-bad-webapp.netlify.app",
-  // },
-  // {
-  //   id: 7,
-  //   title: "Quiz App",
-  //   date: "Dec 2020 - Jan 2021",
-  //   description:
-  //     "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
-  //   image:
-  //     "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
-  //   tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
-  //   category: "android app",
-  //   github: "https://github.com/rishavchanda/Quiz-Earn",
-  //   webapp: "https://github.com/rishavchanda/Quiz-Earn",
-  // },
-  // {
-  //   id: 8,
-  //   title: "Face Recognition",
-  //   date: "Jan 2021",
-  //   description:
-  //     "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
-  //   image:
-  //     "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
-  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-  //   category: "machine learning",
-  //   github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-  //   webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-  // },
+
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
+  { year: 2022, text: "Started my journey" },
+  { year: 2022, text: "Started learning MERN Stack" },
+  { year: 2023, text: "Worked on my first project" },
+  { year: 2024, text: "Started my internship" },
+  { year: 2025, text: "Looking for a full-time job" },
 ];
